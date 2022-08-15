5978c981dbd6b709197a157e83c08a982ce44f02
Menno Tempelaar <menno@hey.com>
2022-08-15 17:42:45 +0200

    Added dutch translations
    And fixed a small typo in the english translations.
    
    Signed-off-by: Menno Tempelaar <menno@hey.com>

---
+++ lang/nl/resource.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    'label' => 'Berichten',
+    'singularLabel' => 'Bericht',
+
+    'fields' => [
+        'general-panel' => 'Algemene gegevens van het bericht',
+
+        'title' => 'Titel',
+        'title-help' => 'De lengte van een goede titel is bij voorkeur 60 tekens lang. Houd in gedachten dat Google slechts 70 karakters kan tonen, en het absolute maximum is ongeveer 120 tekens.',
+
+        'slug' => 'Slug',
+        'slug-help' => 'De slug wordt gebruikt in de url van het bericht en zorgt ervoor dat de url alleen naar dit bericht gaat.',
+
+        'author' => 'Auteur',
+        'author-help' => 'De auteur is de gebruiker die dit bericht heeft geschreven.',
+
+        'poster' => 'Poster',
+        'poster-help' => 'Deze afbeelding zal worden getoond als hoofdafbeelding voor de post.',
+
+        'contents' => 'Inhoud',
+
+        'visibility-panel' => 'Zichtbaarheidsinstellingen',
+        'visibility-panel-help' => 'Instellingen die de zichtbaarheid van het bericht op de website aangeven.',
+
+        'hidden' => 'Het bericht onzichtbaar maken?',
+        'hidden-help' => 'Het bericht wordt niet op de website weergegeven als dit veld is aangevinkt.',
+
+        'published-at' => 'Publiceer bericht op',
+        'published-at-help' => '',
+
+        'published-till' => 'Publiceer bericht tot',
+        'published-till-help' => '',
+
+        'technical-panel' => 'Technische gegevens',
+
+        'updated' => 'Laatste gewijzigd op',
+        'created' => 'Aangemaakt op',
+        'created-by' => 'Aangemaakt door',
+        'deleted' => 'Verwijderd op',
+
+        'index-stack' => 'Berichten',
+
+    ],
+
+];
